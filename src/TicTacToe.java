91e1cc750ec73bfb5651f08d7c4b63014a07ef75
moonshine1l <norka5imen@gmail.com>
2022-08-07 17:49:01 +0300

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,133 @@
+import java.util.*;
+
+public class TicTacToe {
+
+    static ArrayList<Integer> playerPosition = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPosition = new ArrayList<Integer>();
+    public static void main(String[] args) {
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+
+        printGameBoard(gameBoard);
+
+
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите число места (1-9):");
+            int playerPos = scan.nextInt();
+            while (playerPosition.contains(playerPos) || cpuPosition.contains(playerPosition)){
+                System.out.println("Данное место уже занято!");
+                playerPos = scan.nextInt();
+            }
+
+            placePiece(gameBoard, playerPos, "player");
+
+            Random random = new Random();
+            int cpuPos = random.nextInt(9) + 1;
+
+            while (playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos)){
+                System.out.println("Данное место уже занято!");
+                cpuPos = random.nextInt(9) + 1;
+            }
+            placePiece(gameBoard, cpuPos, "cpu");
+
+            printGameBoard(gameBoard);
+
+            String result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+        }
+    }
+
+    public static void printGameBoard(char[][] gameBoard){
+        for (char[] row : gameBoard){
+            for (char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void placePiece(char [][] gameBoard, int pos, String user){
+
+        char symbol = ' ';
+
+        if(user.equals("player")){
+            symbol = 'X';
+            playerPosition.add(pos);
+        } else if(user.equals("cpu")){
+            symbol = 'O';
+            cpuPosition.add(pos);
+        }
+
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static String checkWinner(){
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List leftCross = Arrays.asList(1, 5, 9);
+        List rightCross = Arrays.asList(3, 5, 7);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(leftCross);
+        winning.add(rightCross);
+
+        for (List l : winning){
+            if(playerPosition.containsAll(l)){
+                return "Победа";
+            } else if(cpuPosition.containsAll(l)){
+                return "Поражение";
+            } else if(playerPosition.size() + cpuPosition.size() == 9){
+                return "Ничья";
+            }
+        }
+
+        return "";
+    }
+}
